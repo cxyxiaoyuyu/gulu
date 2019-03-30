7292c937233ab5e002bef44cd0d23df46898d53e
xiaoyu <923996381@qq.com>
2019-03-30 23:46:55 +0800

    add button-group style

--- src/button-group.vue
+++ src/button-group.vue
@@ -20,11 +20,19 @@
             border-radius: 0;
             border-top-left-radius: 4px;
             border-bottom-left-radius: 4px;
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
         }
         &>.button:last-child{
             border-radius: 0;
             border-top-right-radius: 4px;
             border-bottom-right-radius: 4px;
+            &>:hover {
+                position: relative;
+                z-index: 1;
+            }
         }
     }
 </style>
