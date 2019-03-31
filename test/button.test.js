64040c90cbbb38270bf058039ab2817f867e02c9
xiaoyu <923996381@qq.com>
2019-03-31 16:09:53 +0800

    six button unit test success

--- test/button.test.js
+++ test/button.test.js
@@ -20,4 +20,60 @@ const expect = chai.expect;
         expect(useElement.getAttribute('xlink:href')).to.equal('#icon-setting')
         vm.$destroy()
     })
+    it('可以设置loading.', () => {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+        propsData: {
+            iconName: 'setting',
+            loading: true
+        }
+        }).$mount()
+        const useElements = vm.$el.querySelectorAll('use')
+        expect(useElements.length).to.equal(1)
+        expect(useElements[0].getAttribute('xlink:href')).to.equal('#icon-loading')
+        vm.$destroy()
+    })
+    it('icon 默认的 order 是 1', () => {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+        propsData: {
+            iconName: 'setting',
+        }
+        }).$mount(div)
+        const icon = vm.$el.querySelector('svg')
+        expect(getComputedStyle(icon).order).to.eq('0')
+        vm.$el.remove()
+        vm.$destroy()
+    })
+    it('设置 iconPosition 可以改变 order', () => {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                conName: 'setting',
+                iconPosition: 'icon-right'
+            }
+        }).$mount(div)
+        //const icon = vm.$el.querySelector('svg')
+        expect(getComputedStyle(vm.$el).flexDirection).to.eq('row-reverse')
+        vm.$el.remove()
+        vm.$destroy()
+    })
+    it('点击 button 触发 click 事件', () => {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+        propsData: {
+            iconName: 'setting',
+        }
+        }).$mount()
+
+        const callback = sinon.fake();
+        vm.$on('click', callback)
+        vm.$el.click()
+        expect(callback).to.have.been.called
+
+    })
 })
