77ea14b9bbef4d31b3b74e62266a1bd4b2e1e298
xiaoyu <923996381@qq.com>
2019-04-04 00:44:57 +0800

    export input in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Button from './src/button'
 import ButtonGroup from './src/button-group'
 import Icon from './src/icon'
+import Input from './src/input'
 
-export { Button,ButtonGroup,Icon }
+export { Button,ButtonGroup,Icon,Input }
