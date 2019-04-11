f96faf1397de03df519eb4d81a9144dd90d537a5
xiaoyu <923996381@qq.com>
2019-04-12 00:02:04 +0800

    add sidebar

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  themeConfig: {
+    sidebar: [
+      {
+        title: '入门',
+        children: [
+            '/install/',
+            '/get_start/'
+        ]
+      },
+      {
+        title: '组件',
+        children: [
+            '/install/',
+            '/get_start/'
+        ]
+      },
+    ]
+  }
+}
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,4 @@
-# Hello VuePress
+# Gulu UI
+### Gulu UI 是一个轻量级的移动端&PC端的UI组件
+
+
