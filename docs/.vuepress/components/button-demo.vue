86660c7a98541dd844839e34df6d0381893c574a
xiaoyu <923996381@qq.com>
2019-04-14 16:14:42 +0800

    reformat code

--- docs/.vuepress/components/button-demo.vue
+++ docs/.vuepress/components/button-demo.vue
@@ -26,7 +26,7 @@ export default {
                 <g-button icon-name="setting" icon-position="icon-right" :loading="loading2" @click="loading2=!loading2">设置</g-button>
                 <g-button icon-name="download" icon-position="icon-right" :loading="loading3" @click="loading3=!loading3">下载</g-button>
                 <g-button disabled>下载</g-button>
-            `.replace('\t+\g','').trim()
+            `.replace(/\t+| +/g,'').trim()
 
         }
     },
