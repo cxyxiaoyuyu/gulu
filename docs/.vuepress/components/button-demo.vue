49ce19d3513e862789b26e21175a1faf3207e89a
xiaoyu <923996381@qq.com>
2019-04-14 15:23:30 +0800

    add button-demo component

---
+++ docs/.vuepress/components/button-demo.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <g-button>按钮</g-button>
+        <g-button icon-name="setting">按钮</g-button>
+        <g-button icon-name="setting" :loading="loading1">加载</g-button>
+        <g-button icon-name="setting" icon-position="icon-right" :loading="loading2" @click="loading2=!loading2">设置</g-button>
+        <g-button icon-name="download" icon-position="icon-right" :loading="loading3" @click="loading3=!loading3">下载</g-button>
+        <g-button disabled>下载</g-button>
+        
+        <pre><code>{{ content }}</code></pre>
+    </div>
+</template>
+
+<script>
+import Button from '../../../src/button.vue'
+export default {
+    data(){
+        return {
+            loading1: true,   
+            loading2: false,   
+            loading3: false,   
+            content: `
+                <g-button>按钮</g-button>
+                <g-button icon-name="setting">按钮</g-button>
+                <g-button icon-name="setting" :loading="loading1">加载</g-button>
+                <g-button icon-name="setting" icon-position="icon-right" :loading="loading2" @click="loading2=!loading2">设置</g-button>
+                <g-button icon-name="download" icon-position="icon-right" :loading="loading3" @click="loading3=!loading3">下载</g-button>
+                <g-button disabled>下载</g-button>
+            `.replace('\t+\g','').trim()
+
+        }
+    },
+    components: {
+        'g-button': Button
+    },
+}
+</script>
+
+    
--- docs/button/README.md
+++ docs/button/README.md
@@ -2,4 +2,11 @@
 title: 按钮
 ---
 
-按钮
+# 按钮
+
+预览
+
+<button-demo></button-demo>
+
+
+
